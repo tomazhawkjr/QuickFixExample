70f87066c92712a1a19ac920c865cd10d0dced90
Tomaz Junior <tomazhawkjr@hotmail.com>
2023-12-19 19:30:58 -0300

    adição do arquivo de log ignorado pelo git ignore

---
+++ OrderLibrary/Domain/Log/LogMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderLibrary.Domain.Log
+{
+    public class LogMessage
+    {
+        public string Chave { get; set; }      
+        public object Dados { get; set; }
+        public string Erro { get; set; }
+        public DateTime DataHora { get; set; }
+
+    }
+}
