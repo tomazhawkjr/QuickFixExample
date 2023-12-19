185739fad30c375b33da1414d0783aae1d3965fc
Tomaz Junior <tomazhawkjr@hotmail.com>
2023-12-19 19:43:06 -0300

    adicionado arquivo ignorado pelo git ignore

---
+++ OrderGenerator/Domain/Log/LogMessageKeys.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderGenerator.Domain.Log
+{
+    public static class LogMessageKeys
+    {
+        public static string NEW_ORDER_SINGLE_KEY = "ORDERGENERATOR.NEWORDERSINGLE.MENSAGEMENVIADA";
+        public static string NEW_ORDER_SINGLE_KEY_ERRO = "ORDERGENERATOR.NEWORDERSINGLE.MENSAGEMENVIADA.ERRO";
+        public static string NEW_ORDER_SINGLE_KEY_REPORT = "ORDERGENERATOR.NEWORDERSINGLE.RESPOSTARECEBIDA.ACEITO";
+        public static string NEW_ORDER_SINGLE_KEY_REJECT = "ORDERGENERATOR.NEWORDERSINGLE.RESPOSTARECEBIDA.REJEITADO";
+    }
+}
---
+++ OrderReceiver/Domain/Log/LogMessageKeys.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderReceiver.Domain.Log
+{
+    public static class LogMessageKeys
+    {
+        public static string NEW_ORDER_SINGLE_KEY_RECEIVED = "ORDERRECEIVER.NEWORDERSINGLE";
+        public static string NEW_ORDER_SINGLE_KEY_RECEIVED_ERRO = "ORDERRECEIVER.NEWORDERSINGLE.ERRO";
+        public static string NEW_ORDER_SINGLE_KEY_RECEIVED_RESPONSE_ACEITO = "ORDERRECEIVER.NEWORDERSINGLE.RESPONSE.ACEITO";
+        public static string NEW_ORDER_SINGLE_KEY_RECEIVED_RESPONSE_REJEITADO = "ORDERRECEIVER.NEWORDERSINGLE.RESPONSE.REJEITADO";
+    }
+}
